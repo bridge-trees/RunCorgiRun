c7a647b065ec7fd82c3c84b8ab4574fac4e6b0ca
bridge-trees <brrees@chapman.edu>
2026-09-24 15:47:01 -0700

    fixed corgi movement

--- Assets/Scripts/Corgi.cs
+++ Assets/Scripts/Corgi.cs
@@ -11,7 +11,22 @@ public class Corgi : MonoBehaviour
 
     public void Move(Vector2 direction)
     {
-        corgiSpriteRenderer.transform.Translate(direction);
+        FaceCorrectDirection(direction);
+        Vector2 movement = direction * GameParameters.CorgiMoveSpeed * Time.deltaTime;
+        corgiSpriteRenderer.transform.Translate(movement);
+
+        corgiSpriteRenderer.transform.position = SpriteTools.ConstrainToScreen(corgiSpriteRenderer);
+    }
+
+    public void FaceCorrectDirection(Vector2 direction)
+    {
+        if (direction.x > 0)
+        {
+            corgiSpriteRenderer.flipX = false;
+        } else if (direction.x < 0)
+        {
+            corgiSpriteRenderer.flipX = true;
+        }
     }
 
 }
